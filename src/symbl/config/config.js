4012626633121bdeae6eefc77878a69f89bbcb71
Subodh Jena <jenasubodh@gmail.com>
2022-06-28 17:49:05 +0530

    Test account

--- src/symbl/config/config.js
+++ src/symbl/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    appId: '',
-    appSecret: '',
+    appId: '6f365733504b7548367270315550716345614363474a797031794b436e4c5452',
+    appSecret: '6c55645a5f394b594c394144692d6c436e557732516437615449455f6a3949744c686748764e594d6d71383178346d3036573374645f6f5969325068612d756e',
     generateTokenEndpoint: 'https://api.symbl.ai/oauth2/token:generate',
     wssBasePath: 'wss://api.symbl.ai',
     apiBasePath: 'https://api.symbl.ai',
